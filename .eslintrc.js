31428ce31436978ed675b0a7d3779ebe3846aa24
Academy <academy@admins-MacBook-Pro.local>
2019-10-09 15:55:40 +0300

    config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      arrowFunctions: true,
+      classes: true,
+      spread: true,
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true,
-            "modules": true,
-            "arrowFunctions": true,
-            "classes": true,
-            "spread": true,
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "prettier",
-        "eslint-plugin-flowtype"
-    ],
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "prettier/prettier": ["error"],
-    }
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "eslint-plugin-flowtype"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "prettier/prettier": ["error"],
+  },
 };
