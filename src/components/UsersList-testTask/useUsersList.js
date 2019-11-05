08097125fd9c041fe8c2545d34bcfef85b9a8fec
Dmytro Dubinets <18703715+Shizopo@users.noreply.github.com>
2019-11-05 09:52:26 +0200

    wrap all functions in useUsersList in useCallback hook

--- src/components/UsersList-testTask/useUsersList.js
+++ src/components/UsersList-testTask/useUsersList.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useUsersList = () => {
   const [users, setUsers] = useState([]);
   const [userInput, setUserInput] = useState("");
   const [isSomethingSelected, setIsSomethingSelected] = useState(false);
 
-  const fetchData = num => {
+  const fetchData = useCallback(num => {
     fetch(`https://randomuser.me/api/?results=${num}&inc=name`)
       .then(resp => resp.json())
       .then(resp => {
@@ -20,17 +20,17 @@ export const useUsersList = () => {
         setUsers(currentData.slice());
       })
       .catch(err => console.log(err));
-  };
+  }, []);
 
   useEffect(() => {
     fetchData(10);
+  }, [fetchData]);
+
+  const handleInput = useCallback(value => {
+    setUserInput(value);
   }, []);
 
-  const handleInput = value => {
-    setUserInput(value);
-  };
-
-  const onAdd = () => {
+  const onAdd = useCallback(() => {
     let currentData = [...users];
     let text = userInput;
     let key =
@@ -40,24 +40,27 @@ export const useUsersList = () => {
       { first: text, key: key, isChecked: false },
     ]);
     setUserInput("");
-  };
+  }, [userInput, users]);
 
-  const onDelete = () => {
+  const onDelete = useCallback(() => {
     let currentData = [...users];
     let selectedItems = currentData.filter(el => el.isChecked !== true);
 
     setUsers(selectedItems);
     setIsSomethingSelected(false);
-  };
+  }, [users]);
 
-  const handleCheckbox = (val, key) => {
-    let currentData = [...users];
-    let selectedItemIndex = currentData.findIndex(el => el.key === key);
-    currentData[selectedItemIndex].isChecked = val;
+  const handleCheckbox = useCallback(
+    (val, key) => {
+      let currentData = [...users];
+      let selectedItemIndex = currentData.findIndex(el => el.key === key);
+      currentData[selectedItemIndex].isChecked = val;
 
-    setUsers(currentData);
-    setIsSomethingSelected(true);
-  };
+      setUsers(currentData);
+      setIsSomethingSelected(true);
+    },
+    [users]
+  );
 
   return {
     users,
